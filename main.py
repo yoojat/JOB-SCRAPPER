a6ab70ee82fcd8266327e1f362a0fbc91c095d40
태영 <>
2022-11-19 01:29:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from requests import get
+
+base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
+search_term = "python"
+# 다양한 검색을 위해 변수 선언
+
+response = get(f"{base_url}{search_term}")
+# f를 사용하면 문자열 안에 변수를 넣을 수 있음
+
+print(response)
