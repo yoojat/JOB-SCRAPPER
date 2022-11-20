8a276e1ffbf7005c0f9af47ad690ea71557ed76c
kimtaeyoung <35101814+yoojat@users.noreply.github.com>
2022-11-20 05:03:50 +0000

    job list를 가지고 오기

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ if response.status_code != 200:
   print("Can't request website.")
 else:
   soup = BeautifulSoup(response.text, 'html.parser')
-  print(
-    soup.find_all('section', class_='jobs')
-  )  # job class를 가진 section을 찾는 것, class_라고 적는 것 주의(class라는 키워드를 이미 파이썬에서 사용중이기 때문)
+  jobs = soup.find_all('section', class_='jobs')
+  # print(len(jobs))  # 길이 출력
+  for job_section in jobs:
+    job_posts = (job_section.find_all('li'))  # jobs section안에 있는 li를 다가지고 옴
+    job_posts.pop(-1) # 마지막 항목 제거
+
+    for post in job_posts:
+      print(post)
+      print("//////")
+  # job class를 가진 section을 찾는 것, class_라고 적는 것 주의(class라는 키워드를 이미 파이썬에서 사용중이기 때문)
